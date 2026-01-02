6485e0b375805f6d0fe867a6812d1a959d87e3ba
ujjavalparmar <ujjavalparmar2002@gmail.com>
2026-01-03 01:23:51 +0530

    creating task and handling edge cases

--- store.go
+++ store.go
@@ -2,11 +2,34 @@ package main
 
 import "database/sql"
 
-type Store interface{
-
+type Storage struct {
+	db *sql.DB
+}
+type Store interface {
 }
 
 func NewStore(db *sql.DB) Store {
 	return nil
 }
 
+func (s *Storage) CreateUser() error {
+	return nil
+}
+
+func (s *Storage) CreateTask(t *Task) (*Task, error) {
+	rows, err := s.db.Exec("INSERT INTO tasks (name, status, projects_id, assigned_to) VALUES (?,?,?,?)", t.Name, t.Status, t.ProjectID, t.AssignedToID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := rows.LastInsertId()
+
+	if err != nil {
+		return nil, err
+	}
+
+	t.ID = id
+	return t, nil
+
+}
--- tasks.go
+++ tasks.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+var errNameRequired = errors.New("Name is mandatory")
+var errProjectIDRequired = errors.New("Project ID is required")
+var errUserIDRequired = errors.New("User id is required")
 
 type TaskServices struct {
 	store Store
@@ -31,7 +36,17 @@ func (s *TaskServices) handleCreateTask(w http.ResponseWriter, r *http.Request) 
 	var task *Task
 
 	err = json.Unmarshal(body, &task)
-	if err != nil{
+	if err != nil {
+		return
+	}
+
+	if err := validateTaskPayload(task); err != nil {
+		return
+	}
+
+	t, err := s.store.CreateTask(task)
+
+	if err != nil {
 		return
 	}
 
@@ -39,4 +54,20 @@ func (s *TaskServices) handleCreateTask(w http.ResponseWriter, r *http.Request) 
 
 func (s *TaskServices) handleGetTask(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func validateTaskPayload(task *Task) error {
+	if task.Name == "" {
+		return errNameRequired
+	}
+
+	if task.ProjectID == 0 {
+		return errProjectIDRequired
+	}
+
+	if task.AssignedToID == 0 {
+		return errUserIDRequired
+	}
+
+	return nil
 }
